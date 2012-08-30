f8119f739cd66581c84f1da632069a9596e646cb
Jennifer Mitchell <jennifer.mitchell@tealium.com>
2012-08-30 16:37:17 -0700

    Revert "add ssh keys"
    This reverts commit 0ab9501c70256b43939f35c41a1bc98d322ab74d.

--- files/default/wrapssh4git.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export HOME=/tmp/private_code
-/var/lib/jenkins/ssh -q -o "StrictHostKeyChecking=no" -i "/tmp/private_code/.ssh/id_rsa" $1 $2
-/var/lib/jenkins/ssh -q -o "StrictHostKeyChecking=no" -i "/tmp/private_code/.ssh/id_rsa.pub" $1 $2
-
