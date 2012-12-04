e7c90e9fc29c6fefadb1a4d9dcb5f5f0c4f3b84e
Jen Frisk <jennifer.mitchell@tealium.com>
2012-12-04 14:46:47 -0800

    Updated chef-jenkins to install llnw script and perl modules.  This will allow us to run the update-llnw.pl files from jenkins instead of on the utui server.
    Also am having chef create a .json file for each different environment so we can make sure items are uploaded to the correct limelight locations.

---
+++ recipes/llnw_packages.rb
@@ -0,0 +1,99 @@
+#
+# Cookbook Name:: utui
+# Recipe:: llwn_packages
+#
+# Author:: Gautam Dey <gautam@tealium.com>
+#
+# Copyright 2012, Tealium Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "apt"
+include_recipe "perl"
+
+# Install packages that are needed.
+
+%w(
+  libmoose-perl
+  libyaml-perl
+  libjson-perl
+  libcgi-session-perl
+  libmime-lite-perl
+  libnet-amazon-s3-perl
+  libarchive-zip-perl
+  libdatetime-perl
+  libdatetime-format-iso8601-perl
+  libsoap-lite-perl
+  liblog-log4perl-perl
+  libjavascript-minifier-perl
+  libjavascript-minifier-xs-perl
+  libmoosex-attributehelpers-perl
+  libmoosex-log-log4perl-perl
+  libmongodb-perl
+  libtemplate-perl
+  iamcli
+  libipc-run3-perl
+  make
+  libconfig-json-perl
+).each do |pak|
+    package pak do 
+       action :install 
+    end
+end
+
+%w(
+   Net::LimeLight::Purge
+).each do |package| 
+   cpan_module package
+end
+
+directory "/etc/tealium" do 
+   recursive true
+end
+
+directory "/usr/sbin/tealium" do 
+   recursive true
+end
+
+directory "/var/run/tealium" do 
+   recursive true
+   mode 0777
+end
+
+#do not need this right now.  We are using hte one that is in the GitHub repo.
+#cookbook_file "/usr/sbin/tealium/update-llnw.pl" do
+#    source "update-llnw.pl"
+#    mode 0555
+#end
+
+node["update_llnw"].each do |environment|
+  template "/etc/tealium/#{environment.first}.json"  do
+     source "update-llnw.json.erb"
+     mode 0444
+     Chef::Log.info("This is what the environment is #{environment.first}")
+
+     variables(
+      :source_dir       => node["update_llnw"]["#{environment.first}"]["llnw"]["source_dirs"],
+      :ftp_username     => node["update_llnw"]["#{environment.first}"]["ftp"]["username"],
+      :ftp_password     => node["update_llnw"]["#{environment.first}"]["ftp"]["password"],
+      :ftp_url          => node["update_llnw"]["#{environment.first}"]["ftp"]["url"],
+      :llnw_shortname   => node["update_llnw"]["#{environment.first}"]["llnw"]["shortname"],
+      :llnw_username    => node["update_llnw"]["#{environment.first}"]["llnw"]["username"],
+      :llnw_password    => node["update_llnw"]["#{environment.first}"]["llnw"]["password"],
+      :llnw_url         => node["update_llnw"]["#{environment.first}"]["llnw"]["url"],
+      :llnw_prepend_dir => node["update_llnw"]["#{environment.first}"]["llnw"]["prepend_dir"]
+     )
+  end
+end
+
