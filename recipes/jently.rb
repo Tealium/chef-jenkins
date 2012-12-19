6825972e72d3d98b4f97d32b66dd877767aaeaee
Jen Frisk <jennifer.mitchell@tealium.com>
2012-12-19 11:10:36 -0800

    Updated version number
    Updated Jently to clone multiple versions of the gem.
    
    Updated the update-llnw.json.erb file to have the full js and css directory.

--- recipes/jently.rb
+++ recipes/jently.rb
@@ -50,54 +50,61 @@ template "/tmp/private_code/wrapssh4git_data.sh" do
   )
 end
 
-template "/tmp/private_code/clone_update.sh"  do
-   source "clone_update.sh.erb" 
-  	owner node[:jenkins][:server][:user]
-  	group node[:jenkins][:server][:group]
-   mode 0555
-   variables(
-      :sshwrapper => '/tmp/private_code/wrapssh4git_data.sh',
-      :directory => "#{node[:jenkins][:jently]}"
-   )
-end
+#we want to be able to have multiple versions of this gem for the different environments.
+node["jently_config"].each do |jently|
 
-execute "/tmp/private_code/clone_update.sh" do
-	action :run
-end
-
-template "#{node[:jenkins][:jently]}/config/config.yaml" do
-	source "jently_config.yaml.erb"
-	owner node[:jenkins][:server][:user]
-  group node[:jenkins][:server][:group]
-  mode 0700
-  jently_config = search(:jently, "id:development").first
-
- variables(
- 	:github_login => jently_config["github_login"],
-	:github_password => jently_config["github_password"],
-	:github_ssh_repository => jently_config["github_ssh_repository"],		
-	:github_polling_interval_seconds => jently_config["github_polling_interval_seconds"],
-	:jenkins_login => jently_config["jenkins_login"],
-	:jenkins_password => jently_config["jenkins_password"],
-	:jenkins_url => jently_config["jenkins_url"],	
-	:jenkins_job_name => jently_config["jenkins_job_name"],	
-	:jenkins_job_timeout_seconds => jently_config["jenkins_job_timeout_seconds"],	
-	:jenkins_polling_interval_seconds => jently_config["jenkins_polling_interval_seconds"],	
-	:testing_branch_name => jently_config["testing_branch_name"],	
-	:tester_username => jently_config["tester_username"],	
-	:tester_comment => jently_config["tester_comment"],
-  :remote_name => jently_config["remote_name"]
- )
-end
-
-template "#{node[:jenkins][:jently]}/run_ruby_script.sh" do
-	source "run_ruby_script.sh.erb"
-  	owner node[:jenkins][:server][:user]
-  	group node[:jenkins][:server][:group]
+  template "/tmp/private_code/clone_update_#{jently}.sh"  do
+    source "clone_update.sh.erb" 
+    owner node[:jenkins][:server][:user]
+    group node[:jenkins][:server][:group]
     mode 0555
-end
+    variables(
+      :sshwrapper => '/tmp/private_code/wrapssh4git_data.sh',
+      :directory => "#{node[:jenkins][:jently]}/#{jently}"
+    )
+  end
 
-execute "#{node[:jenkins][:jently]}/run_ruby_script.sh" do
-	action :run
-end
+  execute "/tmp/private_code/clone_update_#{jently}.sh" do
+    action :run
+  end
 
+  template "#{node[:jenkins][:jently]}/#{jently}/config/config.yaml" do
+    source "jently_config.yaml.erb"
+    owner node[:jenkins][:server][:user]
+    group node[:jenkins][:server][:group]
+    mode 0700
+    Chef::Log.info("This is what the data_bag is #{jently}")
+    jently_config = search(:jently, "id:#{jently}").first
+
+    variables(
+      :github_login => jently_config["github_login"],
+      :github_password => jently_config["github_password"],
+      :github_ssh_repository => jently_config["github_ssh_repository"],   
+      :github_polling_interval_seconds => jently_config["github_polling_interval_seconds"],
+      :jenkins_login => jently_config["jenkins_login"],
+      :jenkins_password => jently_config["jenkins_password"],
+      :jenkins_url => jently_config["jenkins_url"], 
+      :jenkins_job_name => jently_config["jenkins_job_name"], 
+      :jenkins_job_timeout_seconds => jently_config["jenkins_job_timeout_seconds"], 
+      :jenkins_polling_interval_seconds => jently_config["jenkins_polling_interval_seconds"], 
+      :testing_branch_name => jently_config["testing_branch_name"], 
+      :tester_username => jently_config["tester_username"], 
+      :tester_comment => jently_config["tester_comment"],
+      :remote_name => jently_config["remote_name"]
+    )
+  end
+    
+  template "#{node[:jenkins][:jently]}/#{jently}/run_ruby_script.sh" do
+    source "run_ruby_script.sh.erb"
+    owner node[:jenkins][:server][:user]
+    group node[:jenkins][:server][:group]
+    mode 0555
+    variables(
+      :directory => "#{node[:jenkins][:jently]}/#{jently}"
+    )
+  end
+
+  execute "#{node[:jenkins][:jently]}/#{jently}/run_ruby_script.sh" do
+    action :run
+  end
+end
