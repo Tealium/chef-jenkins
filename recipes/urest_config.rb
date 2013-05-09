48e0979964388b3b26e6904302e615202be9acaa
JenRed12 <jennifer.frisk@tealium.com>
2013-05-09 12:01:10 -0700

    fix small syntax error in urest_config

--- recipes/urest_config.rb
+++ recipes/urest_config.rb
@@ -33,7 +33,7 @@ node["urest_config"].each do |config|
       :urest_host      		=> node["urest_config"]["#{config.first}"]["urest_host"],
       :urest_port     		=> node["urest_config"]["#{config.first}"]["urest_port"],
       :urest_users_path     => node["urest_config"]["#{config.first}"]["urest_users_path"],
-      :urest_legacy_path    => node["urest_config"]["#{config.first}"]["urest_legacy_path"],
+      :urest_legacy_path    => node["urest_config"]["#{config.first}"]["urest_legacy_path"]
      )
   end
 end
