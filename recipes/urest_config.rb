6b56a1b5cd630ffe0f3439baa0c2cdffaa456da7
JenRed12 <jennifer.frisk@tealium.com>
2014-03-21 12:08:15 -0700

    somehow reverted this file

--- recipes/urest_config.rb
+++ recipes/urest_config.rb
@@ -23,21 +23,21 @@ directory "etc/tealium/urest" do
   group node[:jenkins][:server][:user]
 end
 
-node["jenkins"]["urest_config"].each do |config|
+node["urest_config"].each do |config|
   template "/etc/tealium/urest/#{config.first}.json"  do
      source "config.json.erb"
      mode 0444
      Chef::Log.info("This is what the config is #{config.first}")
 
      variables(
-      :urest_host      		=> node[:jenkins]["urest_config"]["#{config.first}"]["urest_host"],
-      :urest_port     		=> node[:jenkins]["urest_config"]["#{config.first}"]["urest_port"],
-      :urest_users_path     => node[:jenkins]["urest_config"]["#{config.first}"]["urest_users_path"],
-      :urest_legacy_path    => node[:jenkins]["urest_config"]["#{config.first}"]["urest_legacy_path"],
-      :utag_host    => node[:jenkins]["urest_config"]["#{config.first}"]["utag_host"],
-      :utag_path    => node[:jenkins]["urest_config"]["#{config.first}"]["utag_path"],
-      :recurly_subdomain => node[:jenkins]["urest_config"]["#{config.first}"]["recurly_subdomain"],
-      :bloomfire_host => node[:jenkins]["urest_config"]["#{config.first}"]["bloomfire_host"]
+      :urest_host      		=> node["urest_config"]["#{config.first}"]["urest_host"],
+      :urest_port     		=> node["urest_config"]["#{config.first}"]["urest_port"],
+      :urest_users_path     => node["urest_config"]["#{config.first}"]["urest_users_path"],
+      :urest_legacy_path    => node["urest_config"]["#{config.first}"]["urest_legacy_path"],
+      :utag_host    => node["urest_config"]["#{config.first}"]["utag_host"],
+      :utag_path    => node["urest_config"]["#{config.first}"]["utag_path"],
+      :recurly_subdomain => node["urest_config"]["#{config.first}"]["recurly_subdomain"],
+      :bloomfire_host => node["urest_config"]["#{config.first}"]["bloomfire_host"]
      )
   end
 end
