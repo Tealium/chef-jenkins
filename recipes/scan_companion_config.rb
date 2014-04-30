9592a6e20d07443be34ecddcc4e7e938b3cdd427
JenRed12 <jennifer.frisk@tealium.com>
2014-04-30 14:32:18 -0700

    Update to have scan companion config

---
+++ recipes/scan_companion_config.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: utui
+# Recipe:: scan_companion_config
+#
+# Copyright 2012, Tealium Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require "json"
+include_recipe "perl"
+
+directory "/etc/tealium/scan_companion" do
+   recursive true
+end
+
+node["sc.server"].each do |tags|
+  template "/etc/tealium/scan_companion/#{tags.first}.json"  do
+     source "scan_companion_config.json.erb"
+     mode 0444
+     variables(
+   	   :scan_companion => node["sc.server"]["#{tags.first}"].nil? ? "{}" : node["sc.server"]["#{tags.first}"].to_hash.to_json
+   )
+  end
+end
