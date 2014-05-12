8b9ff19fe701320ab751fb67576cf17da1ad0746
JenRed12 <jennifer.frisk@tealium.com>
2014-05-12 13:06:53 -0700

    Update to add crontabs for data sync to jenkins slaves.

--- recipes/utui_data_cron.rb
+++ recipes/utui_data_cron.rb
@@ -22,7 +22,39 @@ cron "update_data_daily" do
 	minute "0"
 	hour "1"
 	user "jenkins"
-  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/chef-deployment' ubuntu@54.215.1.2:/data/utui/data/ /data"
+  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/chef-deployment' ubuntu@54.215.1.2:/data/utui/data/accounts/ /data/accounts"
+  action node['disable_data_update'] ? :delete : :create
+end
+
+cron "update_data_daily_qa06" do 
+	minute "0"
+	hour "1"
+	user "jenkins"
+  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.215.238.250:/data/utui/data/accounts"
+  action node['disable_data_update'] ? :delete : :create
+end
+
+cron "update_data_daily_qa05" do 
+	minute "0"
+	hour "1"
+	user "jenkins"
+  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.46.110:/data/utui/data/accounts"
+  action node['disable_data_update'] ? :delete : :create
+end
+
+cron "update_data_daily_qa07" do 
+	minute "0"
+	hour "1"
+	user "jenkins"
+  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.93.107:/data/utui/data/accounts"
+  action node['disable_data_update'] ? :delete : :create
+end
+
+cron "update_data_daily_qa08" do 
+	minute "0"
+	hour "1"
+	user "jenkins"
+  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.122.123:/data/utui/data/accounts"
   action node['disable_data_update'] ? :delete : :create
 end
 
