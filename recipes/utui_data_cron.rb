7ecb8f4ef5c426113b195ea5456e42ed7fa2ee7c
JenRed12 <jennifer.frisk@tealium.com>
2014-07-17 11:41:14 -0700

    update cron to use jenkins user.  Also add qa01 and qa06

--- recipes/utui_data_cron.rb
+++ recipes/utui_data_cron.rb
@@ -22,15 +22,15 @@ cron "update_data_daily" do
 	minute "0"
 	hour "1"
 	user "jenkins"
-  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/chef-deployment' ubuntu@54.215.1.2:/data/utui/data/accounts/ /data/accounts"
+  command "jenkins rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/chef-deployment' ubuntu@54.215.1.2:/data/utui/data/accounts/ /data/accounts"
   action node['disable_data_update'] ? :delete : :create
 end
 
-cron "update_data_daily_qa06" do 
+cron "update_data_daily_qa01" do 
 	minute "0"
 	hour "1"
 	user "jenkins"
-  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.215.238.250:/data/utui/data/accounts"
+  command "jenkins rsync -avz --exclude .git --exclude '/lost+found' -x -p -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.93.107:/data/utui/data/accounts"
   action node['disable_data_update'] ? :delete : :create
 end
 
@@ -38,7 +38,15 @@ cron "update_data_daily_qa05" do
 	minute "0"
 	hour "1"
 	user "jenkins"
-  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.46.110:/data/utui/data/accounts"
+	command "jenkins rsync -avz --exclude .git --exclude '/lost+found' -x -p -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.46.110:/data/utui/data/accounts"
+  action node['disable_data_update'] ? :delete : :create
+end
+
+cron "update_data_daily_qa06" do 
+	minute "0"
+	hour "1"
+	user "jenkins"
+  command "jenkins rsync -avz --exclude .git --exclude '/lost+found' -x -p -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.215.238.250:/data/utui/data/accounts"
   action node['disable_data_update'] ? :delete : :create
 end
 
@@ -46,7 +54,7 @@ cron "update_data_daily_qa07" do
 	minute "0"
 	hour "1"
 	user "jenkins"
-  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.219.129.156:/data/utui/data/accounts"
+	command "jenkins rsync -avz --exclude .git --exclude '/lost+found' -x -p -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.219.129.156:/data/utui/data/accounts"
   action node['disable_data_update'] ? :delete : :create
 end
 
@@ -54,7 +62,7 @@ cron "update_data_daily_qa08" do
 	minute "0"
 	hour "1"
 	user "jenkins"
-  command "rsync -avz --exclude .git -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.122.123:/data/utui/data/accounts"
+	command "jenkins rsync -avz --exclude .git --exclude '/lost+found' -x -p -e 'ssh -i /var/run/tealium/chef/.chef/qa.pem' /data/accounts/ ubuntu@54.193.122.123:/data/utui/data/accounts"
   action node['disable_data_update'] ? :delete : :create
 end
 
