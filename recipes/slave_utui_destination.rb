a91cd76a23e0962a53e9cd971b7c9dc4172f9de8
Michael Hicks <michael.hicks@tealium.com>
2015-07-22 14:21:28 -0700

    slave utui data sync dir creation cookbook just flat out didn't work.  Now it flat out does, yay

--- recipes/slave_utui_destination.rb
+++ recipes/slave_utui_destination.rb
@@ -1,9 +1,10 @@
 # for rsync enabled boxes, create the destination dir for the rsync jobs defined in utui_data_cron.rb
-if node[:jenkins]['cron_utui_account_rsync'] do
+if node[:jenkins]['cron_utui_account_rsync']
     directory "/data/utui/data/accounts" do
-    action :create
-    mode 0777
-    owner node[:jenkins][:server][:user]
-    group node[:jenkins][:server][:user]
+        action :create
+        mode 0777
+        owner node[:jenkins][:server][:user]
+        group node[:jenkins][:server][:user]
+        recursive true
     end
 end
