351ec8223eb79613cce8186d7f367eb118baf3a2
JenRed12 <jennifer.mitchell@tealium.com>
2016-01-08 13:26:39 -0800

    Add community host

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.8.21"
+version          "0.8.22"
 
 %w(runit java).each { |cb| depends cb }
 %w(iptables yum apt).each { |cb| recommends cb }
