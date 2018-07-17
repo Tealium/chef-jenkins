72b711c9ac1b000bb892807fded3f123478e1d47
Aaron East <aaron.east@tealium.com>
2018-07-16 20:10:51 -0700

    fixed metadata, should be 31

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.8.32"
+version          "0.8.31"
 
 %w(runit java).each { |cb| depends cb }
 %w(iptables yum apt).each { |cb| recommends cb }
